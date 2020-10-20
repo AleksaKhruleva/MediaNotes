6abff88b70867fc4b3b769e616c85eed78579d44
Aleksa <aleksa.khruleva@gmail.com>
2020-10-20 18:11:52 +0300

    Initial Commit

---
+++ MediaNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediaNotes
+//
+//  Created by Aleksa on 20.10.2020.
+//  Copyright © 2020 Aleksa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
